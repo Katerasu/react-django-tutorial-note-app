3df05232231d503fc596e98445f57cb65a73712b
Katerasu <dkthong2010@gmail.com>
2024-11-21 21:54:23 +0700

    add backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,7 @@ import { ACCESS_TOKEN } from "./constants";
 const apiURL = '/choreo-apis/djangoreactnoteapp/backend/v1'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiURL,
+  baseURL: apiURL,
 });
 
 // Add a request interceptor to the axios instance
